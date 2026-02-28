19214b591ff31d4d2ffc40bc627c0ace623bd558
Dane Murphy <danem7@gmail.com>
2026-02-28 01:15:31 -0800

    Adjust include path

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 /*
-#cgo CFLAGS: -I${SRCDIR}
-#cgo LDFLAGS: -L${SRCDIR} -L${SRCDIR}/rootfs/usr/lib -lhal_mta -llattice -litc_rpc -lnetsnmp -lsysevent -lhal_util -lmocactl -lssl -lcrypto
-
 #include "mta_hal.h"
 */
 import "C"
