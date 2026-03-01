fe79eef5920937a7145da0845fbfbb3e8b0e89da
Dane Murphy <danem7@gmail.com>
2026-02-28 21:10:35 -0800

    Print a field from a struct in the HAL

--- main.go
+++ main.go
@@ -14,4 +14,5 @@ func main() {
 	var mtaDHCPInfo C.MTAMGMT_MTA_DHCP_INFO
 	C.mta_hal_GetDHCPInfo(&mtaDHCPInfo)
 	fmt.Println("Called the HAL!")
+	fmt.Printf("MAC address: %s\n", C.GoString(&mtaDHCPInfo.MACAddress[0]))
 }
